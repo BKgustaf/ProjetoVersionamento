c1cd0d669a08915c3d3097d391764041c69b71d1
BKgustaf <gustavo.lacerda@iesb.edu.br>
2024-09-21 20:31:58 -0300

    Adicionar estrutura de dados e objeto estudos

---
+++ app.js
@@ -0,0 +1,10 @@
+const diasDaSemana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
+const estudos = {
+  'Segunda-feira': { manha: '', tarde: '', noite: '' },
+  'Terça-feira': { manha: '', tarde: '', noite: '' },
+  'Quarta-feira': { manha: '', tarde: '', noite: '' },
+  'Quinta-feira': { manha: '', tarde: '', noite: '' },
+  'Sexta-feira': { manha: '', tarde: '', noite: '' },
+  'Sábado': { manha: '', tarde: '', noite: '' },
+  'Domingo': { manha: '', tarde: '', noite: '' }
+};
